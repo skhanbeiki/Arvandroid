7c2480a58b6d428760ba5bc16729cbf4e6c2c96a
Moslem Khanbeiki <s.khanbeiki@gmail.com>
2025-01-27 10:14:09 +0330

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://maven.myket.ir")
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.myket.ir")
+    }
+}
+
+rootProject.name = "Arvandroid"
+include(":app")
+include(":watch")
